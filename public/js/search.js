e5f32e93bccf56e5ef787a862d80fc4f281b01fc
Christi Kerr <christinakerr394@gmail.com>
2020-11-03 16:49:56 -0600

    Converted video embed to template literal

--- public/js/search.js
+++ public/js/search.js
@@ -11,8 +11,13 @@ async function generateSearch(search) {
     const result = await searchResults;
     bodyEl.clear;
 
-    result.items.forEach(video => {
-        const videoEmbed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + video.id.videoId + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    result.items.forEach(video => { // Add button to videoEmbed
+        const url = "https://www.youtube.com/embed/" + video.id.videoID
+        const videoEmbed = `
+        <div class="video">
+            <iframe width="560" height="315" src="${url} id=${url} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>`
+
         bodyEl.append(videoEmbed);
     })
 }
