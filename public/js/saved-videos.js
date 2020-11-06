88afa441b196a75906acd2cdbf49d8270a216f5d
Christi Kerr <christinakerr394@gmail.com>
2020-11-06 11:14:27 -0600

    Added space between saved videos

--- public/js/saved-videos.js
+++ public/js/saved-videos.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
           <div class="video">
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive" src="${url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <br>
             </div>  
           </div>`
           bodyEl.append(videoEmbed);
